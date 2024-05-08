972ab696a0a9d1df0421c0b4b1c47bca55359dbd
Jisha <jisha.emmanuvel@comorin.co>
2024-05-08 13:14:02 +0530

    Attendance punchIn api changes

--- src/attendence/attendance.service.ts
+++ src/attendence/attendance.service.ts
@@ -95,12 +95,9 @@ export class AttendanceService {
       Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
-
-    console.log('distance', distance);
+    const distance = R * c;
     return distance;
   }
-
 
   private deg2rad(deg: number): number {
     return deg * (Math.PI / 180);
